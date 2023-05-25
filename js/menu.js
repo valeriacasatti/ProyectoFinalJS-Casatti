74b1f1aa64aa748c021e1f039d2d871b9f85f180
Valeria Casatti <valeria.casatti@gmail.com>
2023-05-24 21:39:40 -0300

    version mobile

---
+++ js/menu.js
@@ -0,0 +1,16 @@
+//DOM
+const openMenu = document.querySelector("#openMenu");
+const closeMenu = document.querySelector("#closeMenu");
+const aside = document.querySelector("aside");
+
+openMenu.addEventListener("click", () =>{
+    aside.classList.add("asideVisible");
+});
+
+closeMenu.addEventListener("click", () =>{
+    aside.classList.remove("asideVisible");
+});
+
+botonesCategorias.forEach(boton => boton.addEventListener("click", () =>{
+    aside.classList.remove("asideVisible");
+}));
